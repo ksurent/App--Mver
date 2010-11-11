a8303bd48362bce0f29636932724566b0e088fc5
Alexey Surikov <ksurent@gmail.com>
2010-11-11 13:18:46 +0300

    first commit

---
+++ lib/App/Mver.pm
@@ -0,0 +1,33 @@
+package App::Mver;
+
+our $VERSION = '0.03';
+
+# vim: ft=perl
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+App::Mver - just print modules' $VERSION
+
+=head1 DESCRIPTION
+
+For those, who are sick of
+
+    perl -MLong::Module::Name -le'print Long::Module::Name->VERSION'
+
+=head1 SYNOPSIS
+
+    mver Module::Name
+
+    mver Module-Name
+
+=head1 AUTHOR
+
+Алексей Суриков E<lt>ksuri@cpan.orgE<gt>
+
+=head1 LICENSE
+
+This program is free software, you can redistribute it under the same terms as Perl itself.
